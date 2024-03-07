6f6ee79fec344a0bba49671f7253211082177207
Ayush Patel <address>
2024-03-07 15:54:05 -0500

    change for jenkins

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,9 +3,9 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 7;
-		int b = 9;
-		int c = 10;
+		int a = 11;
+		int b = 13;
+		int c = 14;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
